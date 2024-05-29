466672b5e81f4ddc92ba6e1eaf312c995de515e1
Dzedas ingmanLT <ddzedas@gmail.com>
2024-05-29 19:00:34 +0200

    Add: remove and add cars from admin

--- Components/Classes/Car.cs
+++ Components/Classes/Car.cs
@@ -4,9 +4,18 @@
     {
 
         public int Id { get; set; }
-        public string Model { get; set; } = string.Empty;
-		public string Year { get; set; } = string.Empty;
-		public string Size { get; set; } = string.Empty;
-		public string PricePerDay { get; set; } = string.Empty;
-	}
+        public string Model { get; set; }
+        public string Year { get; set; }
+        public int? Size { get; set; }
+        public string Color { get; set; }
+        public string Gearbox { get; set; }
+        public double? PricePerDay { get; set; }
+        public string City { get; set; }
+        public string ImageUrl { get; set; }
+
+        public bool AllFieldsFilled()
+        {
+            return (Model != null && Year != null && Size != 0 && Color != null && Gearbox != null && PricePerDay != 0 && City != null && ImageUrl != null);
+        }
+    }
 }
