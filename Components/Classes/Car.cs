b2985442f744168bbe935d8c7b3052179f139b5b
star <starbahadin@hotmail.com>
2024-05-29 10:14:20 +0200

    Update

--- Components/Classes/Car.cs
+++ Components/Classes/Car.cs
@@ -3,13 +3,10 @@
     public class Car
     {
 
-        public int id { get; set; }
-        public string Model { get; set; }
-        public string Year { get; set; }
-        public int Size { get; set; }
-        public string gearbox { get; set; }
-        public double PricePerDay { get; set; }
-        public string City { get; set; }
-        public string ImageUrl { get; set; }
-    }
+        public int Id { get; set; }
+        public string Model { get; set; } = string.Empty;
+		public string Year { get; set; } = string.Empty;
+		public string Size { get; set; } = string.Empty;
+		public string PricePerDay { get; set; } = string.Empty;
+	}
 }
