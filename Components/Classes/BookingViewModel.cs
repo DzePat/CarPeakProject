947fc9de73d27b230ea90a0109f783a2b0144203
Dzedas ingmanLT <ddzedas@gmail.com>
2024-06-03 18:46:48 +0200

    Add: remove and check user bookings.

---
+++ Components/Classes/BookingViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CarPeak.Components.Classes
+{
+    public class BookingViewModel
+    {
+        public User User { get; set; }
+        public Booking Booking { get; set; }
+        public Car Car { get; set; }
+    }
+}
